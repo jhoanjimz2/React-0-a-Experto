cf5d5353d18ac9e0cff87d5e7bb06cba71c0a791
jhoan jimenez <jhoanjimz2@gmail.com>
2023-03-06 23:55:39 -0500

    curso completado hecho por Jhoan Jimenez

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,14 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
-import { Navbar } from "../ui";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={ <LoginPage/> } />
-        <Route path="/*" element={ <HeroesRoutes/> } />
+        
+        <Route path="/login" element={
+          <PublicRoute>
+            <LoginPage/> 
+          </PublicRoute>
+        }></Route>
+
+        <Route path="/*" element={ 
+          <PrivateRoute>
+            <HeroesRoutes/>
+          </PrivateRoute>
+        }></Route>
+
       </Routes>
     </>
   )
